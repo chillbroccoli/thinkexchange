6bb6535125e35d771417e12e7e7c31c93c792fdf
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-08 04:07:37 +0100

    update _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Thinkexchange, an application where you can post your project ideas, project ideas for you or someone's next project, made for all of us developers, that cannot come up with idea what to do, what to build."
+        />
+        <link rel="icon" type="image/png" href="/thinkexchange.png" />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
