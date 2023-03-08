39c7692bc0f883c3f48cafe8cb3e87dd7a2522f4
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-08 04:10:34 +0100

    add keys constants

---
+++ src/utils/constants/keys.ts
@@ -0,0 +1,11 @@
+export enum QUERY_KEYS {
+  PROJECT = "project",
+  PROJECTS = "projects",
+  FEED = "feed",
+  PROJECT_STATS = "projectStats",
+  TAGS = "tags",
+  COMMENTS = "comments",
+  USER_DETAILS = "userDetails",
+  USER_PROJECTS = "userProjects",
+  USER_BOOKMARKS = "userBookmarks",
+}
