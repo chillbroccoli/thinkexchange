5fcc3a090a4ce4d39a2255cbf1ab80d200c62065
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-08 04:06:30 +0100

    add Project component

---
+++ src/components/project/Project.tsx
@@ -0,0 +1,75 @@
+import {
+  Badge,
+  Box,
+  createStyles,
+  Flex,
+  Text,
+  Title,
+  TypographyStylesProvider,
+} from "@mantine/core";
+import { useSession } from "next-auth/react";
+
+import { ProjectResponse } from "~/utils/schemas/project.schema";
+
+import { SettingsMenu } from "./SettingsMenu";
+
+export function Project({ project }: { project?: ProjectResponse }) {
+  const { data: session } = useSession();
+
+  const { classes } = styles();
+
+  if (!project) return null;
+
+  const { title, description, content, tags, user } = project;
+
+  return (
+    <Box px={40} p={10} className={classes.main}>
+      {user.id === session?.user.id && <SettingsMenu />}
+
+      <Flex direction="column" mt={14}>
+        <Box>
+          <Title order={1} fw={600}>
+            {title}
+          </Title>
+        </Box>
+
+        <Flex mt={10} gap={10}>
+          {tags.map((tag) => (
+            <Badge key={tag.id} variant="outline" radius="md">
+              # {tag.name}
+            </Badge>
+          ))}
+        </Flex>
+
+        <Box my={12}>
+          <Text color="gray.7" fw={300}>
+            {description}
+          </Text>
+        </Box>
+
+        <Box>
+          <TypographyStylesProvider>
+            <div dangerouslySetInnerHTML={{ __html: content }}></div>
+          </TypographyStylesProvider>
+        </Box>
+      </Flex>
+
+      {/* <Comments /> */}
+    </Box>
+  );
+}
+
+const styles = createStyles((theme) => ({
+  avatar: {
+    textTransform: "uppercase",
+    border: `1px solid ${theme.colors.teal[5]}`,
+  },
+
+  main: {
+    border: `1px solid ${theme.colors.gray[3]}`,
+    borderRadius: theme.radius.sm,
+    boxShadow: theme.shadows.sm,
+    backgroundColor: theme.white,
+    position: "relative",
+  },
+}));
