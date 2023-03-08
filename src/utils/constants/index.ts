89a0c536a98aff3322995cc8729f2e373aee138f
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-08 04:10:30 +0100

    add index constants file

---
+++ src/utils/constants/index.ts
@@ -0,0 +1,7 @@
+import { ClientRoutes } from "./routes";
+
+export const profileNav = [
+  { name: "New Project", href: ClientRoutes.NEW_PROJECT },
+  { name: "My Projects", href: ClientRoutes.MY_PROJECTS },
+  { name: "Bookmarks", href: ClientRoutes.BOOKMARKS },
+];
