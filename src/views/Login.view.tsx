10646629f6b073a8d2b11d3da97c3f0fabaf657d
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-09 11:41:37 +0100

    add redirect url

--- src/views/Login.view.tsx
+++ src/views/Login.view.tsx
@@ -1,15 +1,20 @@
 import { Box, Button, Flex, Title } from "@mantine/core";
 import { IconBrandGithub } from "@tabler/icons-react";
+import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
 
 import { AuthLayout } from "~/components/layouts/AuthLayout";
 import { ClientRoutes } from "~/utils/constants/routes";
 
 export function LoginView() {
+  const router = useRouter();
+
+  const { redirect } = router.query as { redirect?: string };
+
   const githubLogin = () => {
     void signIn("github", {
       redirect: true,
-      callbackUrl: ClientRoutes.HOME,
+      callbackUrl: redirect ? redirect : ClientRoutes.HOME,
     });
   };
 
