c09ad876f5931ba7651998cff9eb942468a35106
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-09 17:48:03 +0100

    add api route for projects tag [tag]

---
+++ src/pages/api/projects/tag/[tag]/index.ts
@@ -0,0 +1,50 @@
+import { NextApiHandler } from "next";
+
+import { prisma } from "~/utils/server/db";
+
+const handler: NextApiHandler = async (req, res) => {
+  const { tag } = req.query as { tag: string };
+
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  try {
+    const projects = await prisma.project.findMany({
+      take: 5,
+      where: {
+        tags: {
+          some: {
+            name: tag,
+          },
+        },
+      },
+      include: {
+        tags: true,
+        likes: true,
+        comments: true,
+        bookmarks: true,
+        _count: true,
+        user: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+          },
+        },
+      },
+      orderBy: [
+        {
+          createdAt: "desc",
+        },
+      ],
+    });
+
+    return res.status(200).json(projects);
+  } catch (err) {
+    return res.status(500).json({ message: "Couldn't load projects" });
+  }
+};
+
+export default handler;
