e6a69ea9bebebac63d216cd40a96cfa03786fcf8
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-09 05:23:50 +0100

    import comment api

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,7 +1,9 @@
+import { comment } from "./services/comment.api";
 import { project } from "./services/project.api";
 import { tag } from "./services/tag.api";
 
 export const api = {
   tag,
   project,
+  comment,
 };
