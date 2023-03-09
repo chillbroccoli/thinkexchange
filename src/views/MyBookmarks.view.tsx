180ea30317fea5ad279da673f1edcde3eb1edb93
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-09 12:04:56 +0100

    add MyBookmarks view

---
+++ src/views/MyBookmarks.view.tsx
@@ -0,0 +1,23 @@
+import { Box, Container } from "@mantine/core";
+
+import { BookmarksNotFound } from "~/components/BookmarksNotFound";
+import { MainLayout } from "~/components/layouts/MainLayout";
+import { Feed } from "~/components/project/Feed";
+import { api } from "~/utils/api";
+
+export function MyBookmarksView() {
+  const { data, isLoading } = api.project.useUserBookmarks();
+
+  return (
+    <MainLayout>
+      <Container size="lg" mt={20}>
+        <Box w="50%" mx="auto">
+          <Box mt={20}>
+            <Feed data={data} isLoading={isLoading} showEmptyState={false} />
+            {!data?.length && <BookmarksNotFound />}
+          </Box>
+        </Box>
+      </Container>
+    </MainLayout>
+  );
+}
