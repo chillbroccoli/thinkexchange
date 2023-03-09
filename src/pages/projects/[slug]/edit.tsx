97b25366358f55635e654cdb9dd90538cbed8b69
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-09 11:40:54 +0100

    add requiredAuth for edit project page

--- src/pages/projects/[slug]/edit.tsx
+++ src/pages/projects/[slug]/edit.tsx
@@ -1,5 +1,10 @@
+import { GetServerSideProps } from "next";
+
+import { requireAuth } from "~/utils/server/requireAuth";
 import { EditProjectView } from "~/views/EditProject.view";
 
 export default function EditProjectPage() {
   return <EditProjectView />;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => requireAuth(context);
