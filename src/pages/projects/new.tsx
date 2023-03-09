44f99a5599461e2fb5fae3d5df324a15be47b2a1
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-09 11:40:44 +0100

    add requiredAuth for new project page

--- src/pages/projects/new.tsx
+++ src/pages/projects/new.tsx
@@ -1,5 +1,10 @@
+import { GetServerSideProps } from "next";
+
+import { requireAuth } from "~/utils/server/requireAuth";
 import { NewProjectView } from "~/views/NewProject.view";
 
 export default function NewProject() {
   return <NewProjectView />;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => requireAuth(context);
