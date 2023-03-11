22a9cd329e226922bd72098f03e232d39f59b626
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-11 09:45:07 +0100

    add showLoader prop

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,12 +1,19 @@
 import { Box } from "@mantine/core";
 
+import { LoaderLayout } from "~/components/layouts/LoaderLayout";
 import { Navbar } from "~/components/Navbar";
 
-export function MainLayout({ children }: { children: React.ReactNode }) {
+export function MainLayout({
+  children,
+  showLoader,
+}: {
+  children: React.ReactNode;
+  showLoader?: boolean;
+}) {
   return (
     <Box>
       <Navbar />
-      <Box pb={100}>{children}</Box>
+      {showLoader ? <LoaderLayout /> : <Box pb={100}>{children}</Box>}
     </Box>
   );
 }
