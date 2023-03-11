444b8103ad1bcd50269d12ad164404d85bd1c17c
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-11 09:44:58 +0100

    update LoaderLayout style

--- src/components/layouts/LoaderLayout.tsx
+++ src/components/layouts/LoaderLayout.tsx
@@ -1,9 +1,21 @@
-import { Flex, Loader } from "@mantine/core";
+import { createStyles, Flex, Loader } from "@mantine/core";
 
 export function LoaderLayout() {
+  const { classes } = styles();
+
   return (
-    <Flex w="100vw" h="100vh" align="center" justify="center">
+    <Flex w="100%" h="100%" align="center" justify="center" className={classes.main}>
       <Loader />
     </Flex>
   );
 }
+
+const styles = createStyles(() => ({
+  main: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+}));
