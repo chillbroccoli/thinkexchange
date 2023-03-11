7ab232761ba6edbdc64dbcad69bf23ae71ae4fbd
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-11 09:45:21 +0100

    remove showing loader from Feed component

--- src/components/project/Feed.tsx
+++ src/components/project/Feed.tsx
@@ -2,20 +2,16 @@ import { Box, Flex } from "@mantine/core";
 
 import { EmptyState } from "~/components/project/EmptyState";
 import { ProjectCard } from "~/components/project/ProjectCard";
-import { Spinner } from "~/components/Spinner";
 import { ProjectResponse } from "~/utils/schemas/project.schema";
 
 export function Feed({
   data,
-  isLoading,
   showEmptyState = true,
 }: {
   data?: ProjectResponse[];
-  isLoading?: boolean;
   showEmptyState?: boolean;
 }) {
-  if (isLoading && !data?.length) return <Spinner />;
-  if (showEmptyState && !isLoading && !data?.length) return <EmptyState />;
+  if (showEmptyState && !data?.length) return <EmptyState />;
 
   return (
     <Box>
