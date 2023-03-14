2449ffb2ea7b5ae357346a69a3df9c801452ce62
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-14 11:06:04 +0100

    handle responsivnes in new project view

--- src/views/NewProject.view.tsx
+++ src/views/NewProject.view.tsx
@@ -1,13 +1,16 @@
 import { Box, Container } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 
 import { NewProjectForm } from "~/components/forms/NewProjectForm";
 import { MainLayout } from "~/components/layouts/MainLayout";
 
 export function NewProjectView() {
+  const smallScreen = useMediaQuery("(min-width: 48em)");
+
   return (
     <MainLayout>
       <Container size="lg" mt={20} pb={20}>
-        <Box mx="auto" w="75%">
+        <Box mx="auto" w={smallScreen ? "75%" : "90%"}>
           <NewProjectForm />
         </Box>
       </Container>
