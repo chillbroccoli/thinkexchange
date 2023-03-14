63cd77a935492f9ec60978686f315c42cb53ea6a
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-14 11:06:26 +0100

    handle responsivnes in tag view

--- src/views/Tag.view.tsx
+++ src/views/Tag.view.tsx
@@ -1,4 +1,16 @@
-import { Button, Container, createStyles, Divider, Flex, Grid, Text, Title } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Container,
+  createStyles,
+  Divider,
+  Flex,
+  Grid,
+  MediaQuery,
+  Text,
+  Title,
+} from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { IconPlus } from "@tabler/icons-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -16,6 +28,7 @@ import { useScrollPosition } from "~/utils/hooks/useScrollPosition";
 export function TagView() {
   const { data: session } = useSession();
   const { classes } = styles();
+  const smallScreen = useMediaQuery("(min-width: 48em)");
 
   const router = useRouter();
 
@@ -55,8 +68,39 @@ export function TagView() {
   return (
     <MainLayout showLoader={isLoading}>
       <Container size="lg" mt={20}>
-        <Grid gutter={10}>
-          <Grid.Col span={3}>
+        <MediaQuery smallerThan="md" styles={{ display: "none" }}>
+          <Grid gutter={10}>
+            <Grid.Col span={3}>
+              <Flex direction="column" className={classes.sidebar} p={10} px={14}>
+                <Title order={2} transform="capitalize" mb={8}>
+                  {router.query?.tag}
+                </Title>
+                {session?.user && (
+                  <Button
+                    component={Link}
+                    href={ClientRoutes.NEW_PROJECT}
+                    leftIcon={<IconPlus size={20} />}
+                  >
+                    New Project
+                  </Button>
+                )}
+
+                <Divider my="sm" />
+
+                <Text>{pluralizeCount(projects.length ?? 0, "post")} found</Text>
+              </Flex>
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Feed data={projects} />
+            </Grid.Col>
+            <Grid.Col span={3}>
+              <MiniList title="Latest" data={latestProjects} />
+            </Grid.Col>
+          </Grid>
+        </MediaQuery>
+
+        <MediaQuery largerThan="md" styles={{ display: "none" }}>
+          <Box w={smallScreen ? "70%" : "90%"} mx="auto">
             <Flex direction="column" className={classes.sidebar} p={10} px={14}>
               <Title order={2} transform="capitalize" mb={8}>
                 {router.query?.tag}
@@ -75,14 +119,12 @@ export function TagView() {
 
               <Text>{pluralizeCount(projects.length ?? 0, "post")} found</Text>
             </Flex>
-          </Grid.Col>
-          <Grid.Col span={6}>
-            <Feed data={projects} />
-          </Grid.Col>
-          <Grid.Col span={3}>
-            <MiniList title="Latest" data={latestProjects} />
-          </Grid.Col>
-        </Grid>
+
+            <Box mt={20}>
+              <Feed data={projects} />
+            </Box>
+          </Box>
+        </MediaQuery>
       </Container>
     </MainLayout>
   );
