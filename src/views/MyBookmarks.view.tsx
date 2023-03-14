7a7bdd39fa42157ba2280bd1af52e2311eea0221
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-14 11:05:46 +0100

    handle responsivnes in my bookmarks view

--- src/views/MyBookmarks.view.tsx
+++ src/views/MyBookmarks.view.tsx
@@ -1,4 +1,5 @@
 import { Box, Container } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 
 import { BookmarksNotFound } from "~/components/BookmarksNotFound";
 import { MainLayout } from "~/components/layouts/MainLayout";
@@ -6,12 +7,13 @@ import { Feed } from "~/components/project/Feed";
 import { api } from "~/utils/api";
 
 export function MyBookmarksView() {
+  const smallScreen = useMediaQuery("(min-width: 48em)");
   const { data, isLoading } = api.project.useUserBookmarks();
 
   return (
     <MainLayout showLoader={isLoading}>
       <Container size="lg" mt={20}>
-        <Box w="50%" mx="auto">
+        <Box w={smallScreen ? "70%" : "90%"} mx="auto">
           <Box mt={20}>
             <Feed data={data} showEmptyState={false} />
             {!data?.length && <BookmarksNotFound />}
