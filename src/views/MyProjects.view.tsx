b99edd460e0ea9e3c7c2080acc5fa6c1ad1ca8ff
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-14 11:05:53 +0100

    handle responsivnes in my projects view

--- src/views/MyProjects.view.tsx
+++ src/views/MyProjects.view.tsx
@@ -1,16 +1,18 @@
 import { Box, Container } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 
 import { MainLayout } from "~/components/layouts/MainLayout";
 import { Feed } from "~/components/project/Feed";
 import { api } from "~/utils/api";
 
 export function MyProjectsView() {
+  const smallScreen = useMediaQuery("(min-width: 48em)");
   const { data, isLoading } = api.project.useUserProjects();
 
   return (
     <MainLayout showLoader={isLoading}>
       <Container size="lg" mt={20}>
-        <Box w="50%" mx="auto">
+        <Box w={smallScreen ? "70%" : "90%"} mx="auto">
           <Box mt={20}>
             <Feed data={data} />
           </Box>
