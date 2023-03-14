86cc3c94f4d4b2ba370dd8d5b344ac7fdd081b6a
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-14 11:25:16 +0100

    update seed file

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -1,4 +1,13 @@
+import { faker } from "@faker-js/faker";
 import { PrismaClient } from "@prisma/client";
+import slugify from "slugify";
+
+export function getSlug(title) {
+  return slugify(title, { lower: true, replacement: "-" }).concat(
+    "-",
+    (Math.random() + 1).toString(36).substring(2)
+  );
+}
 
 const prisma = new PrismaClient();
 
@@ -77,6 +86,8 @@ export const tagList = [
 
 export const seed = async () => {
   await prisma.tag.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.project.deleteMany();
 
   const createTags = tagList.map((tag) =>
     prisma.tag.create({
@@ -86,7 +97,64 @@ export const seed = async () => {
     })
   );
 
-  await prisma.$transaction(createTags);
+  const tags = await prisma.$transaction(createTags);
+
+  const userData = Array(10)
+    .fill(null)
+    .map(() => ({
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      image: faker.internet.avatar(),
+    }));
+
+  const createUsers = userData.map((user) =>
+    prisma.user.create({
+      data: user,
+    })
+  );
+
+  const users = await prisma.$transaction(createUsers);
+
+  const projects = [];
+
+  for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < 3; j++) {
+      projects.push({
+        createdAt: faker.date.past(),
+        title: faker.commerce.productName(),
+        description: faker.lorem.paragraph(8),
+        content: faker.lorem.paragraphs(5),
+        slug: getSlug(faker.lorem.slug()),
+        user: {
+          connect: {
+            id: users[i].id,
+          },
+        },
+        // Connect 3 random tags to each project
+        tags: {
+          connect: [
+            {
+              id: tags[Math.floor(Math.random() * tags.length)].id,
+            },
+            {
+              id: tags[Math.floor(Math.random() * tags.length)].id,
+            },
+            {
+              id: tags[Math.floor(Math.random() * tags.length)].id,
+            },
+          ],
+        },
+      });
+    }
+  }
+
+  const createProjects = projects.map((project) =>
+    prisma.project.create({
+      data: project,
+    })
+  );
+
+  await prisma.$transaction(createProjects);
 };
 
 seed()
