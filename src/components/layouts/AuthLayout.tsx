1f9979331b8dde647abed3f9a0b3d7a5d1b1e8d8
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-14 11:23:59 +0100

    add back button for auth layout

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -1,12 +1,20 @@
-import { Box, createStyles, Flex } from "@mantine/core";
+import { ActionIcon, Box, createStyles, Flex } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+import { IconArrowLeft } from "@tabler/icons-react";
+import { useRouter } from "next/router";
 
 export function AuthLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const maxMediumScreen = useMediaQuery("(max-width: 64em)");
   const { classes } = styles();
 
   return (
     <Box w="100vw" h="100vh">
+      <ActionIcon onClick={() => router.back()} className={classes.backButton}>
+        <IconArrowLeft size={20} />
+      </ActionIcon>
       <Flex align="center" justify="center" w="100%" h="100%">
-        <Box w={400} p={14} className={classes.main}>
+        <Box w={maxMediumScreen ? "85%" : 400} p={14} className={classes.main}>
           {children}
         </Box>
       </Flex>
@@ -20,5 +28,13 @@ const styles = createStyles((theme) => ({
     borderRadius: theme.radius.md,
     boxShadow: theme.shadows.sm,
     backgroundColor: theme.white,
+  },
+
+  backButton: {
+    border: `1px solid ${theme.colors.indigo[5]}`,
+    color: theme.colors.indigo[5],
+    position: "absolute",
+    top: 20,
+    left: 20,
   },
 }));
