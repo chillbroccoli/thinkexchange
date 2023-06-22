a95dd999396e668e1872c017cc33d0dbc7e16bd1
Kacper Malek <kacper.malek0828@gmail.com>
2023-06-22 12:17:13 +0200

    convert from Mantine UI to Tailwind

--- src/views/NewProject.view.tsx
+++ src/views/NewProject.view.tsx
@@ -1,19 +1,12 @@
-import { Box, Container } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
-
 import { NewProjectForm } from "~/components/forms/NewProjectForm";
 import { MainLayout } from "~/components/layouts/MainLayout";
 
 export function NewProjectView() {
-  const smallScreen = useMediaQuery("(min-width: 48em)");
-
   return (
     <MainLayout>
-      <Container size="lg" mt={20} pb={20}>
-        <Box mx="auto" w={smallScreen ? "75%" : "90%"}>
-          <NewProjectForm />
-        </Box>
-      </Container>
+      <div className="w-[90%] sm:w-auto max-w-6xl pb-5 mx-auto mt-5">
+        <NewProjectForm />
+      </div>
     </MainLayout>
   );
 }
